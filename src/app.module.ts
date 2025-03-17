1ce2e570d3c24a8539afe59c854286e8535d8342
Giovane <giovane@rlvtecnologia.com.br>
2025-03-17 19:04:06 -0300

    fea(library): implementa prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksController } from './books/books.controller';
 import { BooksService } from './books/books.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController, BooksController],
-  providers: [AppService, BooksService],
+  providers: [AppService, BooksService, PrismaService],
 })
 export class AppModule {}
